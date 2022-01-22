be9ed6ee057d620b73e00930789ff537ef5bcbaf
Rushikesh Bhoi <73032807+rushikeshbhoi@users.noreply.github.com>
2022-01-23 02:19:24 +0530

    Add files via upload

---
+++ Max_in_arraylist.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"\nEnter input:";
+    cin>>n;
+    int arr[n];
+    cout<<"\nEnter Array elements:";
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+
+    int max = arr[0];
+
+    for(int i=0;i<n;i++)
+    {
+        if(max<arr[i])
+        {
+            max = arr[i];
+        }
+    }
+
+    cout<<"\nThe maximum element in the array list is "<<max;
+
+
+}
